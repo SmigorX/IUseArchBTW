0796dae903bc1daab597c21ce414fbc80f6bcaa8
SmigorX <igorjaniak@gmail.com>
2024-05-31 16:49:57 +0200

    Hyprland configs

---
+++ nvim/lua/mason.lua
@@ -0,0 +1,11 @@
+return {
+    "williamboman/mason.nvim",
+	ui = {
+        icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+        },
+    }
+}
+
